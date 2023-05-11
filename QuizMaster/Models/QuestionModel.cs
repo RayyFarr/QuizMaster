ce843f2bb007a5d4674a3e76c6522b8c09d0f82c
RayyFarr <dohvakiin555@yahoo.com>
2023-05-11 13:49:17 +0600

    Add project files.

---
+++ QuizMaster/Models/QuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace QuizMaster.Models
+{
+	public class QuestionModel
+	{
+		public string? Id { get; set; }
+		public string? Question { get; set; }
+		public string[]? Answers { get; set; }
+		public int? AnswerIndex { get; set; }
+		public string? Hint { get; set; }
+		public string? Contributor { get; set; }
+		public int[]? Ratings { get; set; }
+
+		public override string ToString() => JsonSerializer.Serialize(this);
+	}
+}
---
+++ QuizMaster/Services/JsonFileQuestionService.cs
@@ -0,0 +1,27 @@
+﻿using QuizMaster.Models;
+using System.Text.Json;
+
+namespace QuizMaster.Services
+{
+	public class JsonFileQuestionService
+	{
+		public JsonFileQuestionService(IWebHostEnvironment webHostEnvironment)
+		{
+			WebHostEnvironment = webHostEnvironment;
+		}
+
+		public IWebHostEnvironment WebHostEnvironment { get; }
+
+		private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "questions.json");
+
+		public QuestionModel[] GetQuestions()
+		{
+			using var jsonFileReader = File.OpenText(JsonFileName);
+			return JsonSerializer.Deserialize<QuestionModel[]>(jsonFileReader.ReadToEnd(),
+				new JsonSerializerOptions
+				{
+					PropertyNameCaseInsensitive = true
+				});
+		}
+	}
+}
